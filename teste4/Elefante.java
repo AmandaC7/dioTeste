a77b6f7289a75b2ec01a7ab37aa2d7a2c3e8983e
Amanda <Amandacdesousa@outlook.com>
2020-09-15 16:06:38 -0300

    Funcionando ao contrário

--- teste4/Elefante.java
+++ teste4/Elefante.java
@@ -3,11 +3,21 @@ package dioTeste.teste4;
 import java.util.Scanner;
 
 public class Elefante {
-    public static void main (String[]args) {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        for (0, n){
-            range x (n);
-            System.out.prinln(muito);
+        System.out.println("Digite um número");
+        int i = 0;
+        for (i = scan.nextInt(); i > 0; i --) {
+            if (i == 1){
+                System.out.println("1 Elefante incomoda muita gente");
+            }
+            else if (i > 1){
+                System.out.println(i + " Elefantes incomodam muito mais");
+
+            }
+        }
+
+
+
         }
     }
-}
