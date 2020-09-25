aaf7c38fd175a3d52d2dab03ba763aa38ee5d148
Amanda <Amandacdesousa@outlook.com>
2020-09-24 22:02:01 -0300

    Elefante finalizado

--- teste4/Elefante.java
+++ teste4/Elefante.java
@@ -4,20 +4,16 @@ import java.util.Scanner;
 
 public class Elefante {
     public static void main(String[] args) {
+        int elefanteIncomoda;
         Scanner scan = new Scanner(System.in);
         System.out.println("Digite um número");
-        int i = 0;
-        for (i = scan.nextInt(); i > 0; i --) {
-            if (i == 1){
-                System.out.println("1 Elefante incomoda muita gente");
-            }
-            else if (i > 1){
-                System.out.println(i + " Elefantes incomodam muito mais");
+        elefanteIncomoda = scan.nextInt();
+        System.out.print("1 elefante incomoda muita gente, " + elefanteIncomoda + " elefantes ");
+        for (int i = 1; i <= elefanteIncomoda ; i++) {
+                System.out.print("incomoda ");
 
-            }
         }
-
-
+        System.out.print("muito mais");
 
         }
     }
